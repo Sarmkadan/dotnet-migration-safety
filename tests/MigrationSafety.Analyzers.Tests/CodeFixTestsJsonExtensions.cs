315028ab09538347653aa023deecbbb9ebe20293
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    small refactor - CodeFixTestsJsonExtensions.
    found this while looking at something else.

--- tests/MigrationSafety.Analyzers.Tests/CodeFixTestsJsonExtensions.cs
+++ tests/MigrationSafety.Analyzers.Tests/CodeFixTestsJsonExtensions.cs
@@ -17,14 +17,8 @@ public static class CodeFixTestsJsonExtensions
         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
     };
 
-    private static JsonSerializerOptions GetJsonOptions(bool indented)
-    {
-        var options = new JsonSerializerOptions(_jsonOptions)
-        {
-            WriteIndented = indented,
-        };
-        return options;
-    }
+    private static JsonSerializerOptions GetJsonOptions(bool indented) =>
+        new(_jsonOptions) { WriteIndented = indented };
 
     /// <summary>
     /// Serializes the <see cref="CodeFixTests"/> instance to a JSON string.
@@ -32,7 +26,7 @@ public static class CodeFixTestsJsonExtensions
     /// <param name="value">The code fix tests instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the code fix tests instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><inheritdoc cref="ArgumentNullException" path="/exception"/></exception>
     public static string ToJson(this CodeFixTests value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -45,8 +39,8 @@ public static class CodeFixTestsJsonExtensions
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized code fix tests instance, or null if the JSON represents a null value.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    /// <exception cref="ArgumentNullException"><inheritdoc cref="ArgumentNullException" path="/exception"/></exception>
+    /// <exception cref="JsonException"><inheritdoc cref="JsonException" path="/exception"/></exception>
     public static CodeFixTests? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
@@ -60,7 +54,7 @@ public static class CodeFixTestsJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized code fix tests instance if successful.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><inheritdoc cref="ArgumentNullException" path="/exception"/></exception>
     public static bool TryFromJson(string json, out CodeFixTests? value)
     {
         ArgumentNullException.ThrowIfNull(json);
